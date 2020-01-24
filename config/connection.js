ad1db6d99ee45920df1f98b83b7a08645c33c8bb
Nancy Harris <nanzyharris@gmail.com>
2020-01-23 21:50:53 -0600

    connection changed

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "pearsley03",
-    database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "pearsley03",
+        database: "burgers_db"
+    });
+};
 // Make connection.
 connection.connect(function (err) {
     if (err) {
